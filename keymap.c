f17920ea11defbe83f89cf34e0385796f8a8fba2
Marc St Pierre <marcstpierre93@gmail.com>
2022-01-09 21:26:54 -0800

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 Steven Karrmann
+/* Copyright 2022 Marc St. Pierre
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
